b58da3c9f41780b92c3790c2f5f858e274bba09e
Himanshu <169117364+imphenomenal07@users.noreply.github.com>
2026-03-19 13:44:20 +0530

    Implement FastAPI with home and ask endpoints

---
+++ Python Frontned API (FastAPI)/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import requests
+app = FastAPI()
+OLLAMA_URL = "http://localhost:11434/api/generate"
+@app.get("/")
+def home():
+ return {"message": "Ollama 3B API is running"}
+@app.get("/ask")
+def ask(prompt: str):
+ payload = {
+ "model": "llama3:3b",
+ "prompt": prompt,
+ "stream": False
+ }
+ response = requests.post(OLLAMA_URL, json=payload)
+ return response.json()
